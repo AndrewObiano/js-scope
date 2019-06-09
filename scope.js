72b64729e5746977c75dd5a30f6949479db64177
Andrew Obiano <andrew.obiano@gmail.com>
2019-06-09 12:00:03 -1000

    finished

--- scope.js
+++ scope.js
@@ -2,11 +2,11 @@
 var isCookieJarOpen = null;
 
 function closeLid() {
-  /* answer here */
+  isCookieJarOpen = false;
 }
 
 function openLid() {
-  /* answer here */
+  isCookieJarOpen = true;
 }
 
 
@@ -17,7 +17,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + ' ' + world;
   }
   return innerFunction();
 }
@@ -31,8 +31,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var x = 0; x < matrix[i].length; x++) {
+      result = result + matrix[i][x];
     }
   }
   return result;
@@ -54,11 +54,11 @@ function sendDataToClient() {
       authenticated: false
     };
 
-    if (userObject.handle === username) {
-      userObject.authenticated = true;
-      return userObject
+    if (obj.handle === username) {
+      obj.authenticated = true;
+      return obj
     } else {
-      return userObject
+      return obj
     }
   }
   authenticateUser(userObject, 'neo')
